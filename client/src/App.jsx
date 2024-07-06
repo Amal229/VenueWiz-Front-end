7255a363d46b1bd4e57134e9a3d47dd9533d2bb0
AbeerMajeed <abeermajeed78@gmail.com>
2024-07-06 18:38:28 +0300

    app updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
 import './App.css'
+import Venue from './components/Vendor/AddingVenuesForm'
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { CheckSession } from './services/Auth'
 
 const App = () => {
+  const [user, setUser] = useState({})
+
+  const handleLogOut = () => {
+    // Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div>
       {/* nav */}
       <main>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
+          <Route path="Venue" element={<Venue user={user} />} />
         </Routes>
       </main>
     </div>
